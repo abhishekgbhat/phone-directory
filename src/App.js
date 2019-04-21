0e2193605890c4d2fcab105c1bfa2ebda26ba505
Abhishek G Bhat <abhishek.bhat5@gmail.com>
2019-04-21 10:10:01 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        Phone Directory
+        {/* <div className="header">
+          Phone Directory
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span>
+          <br></br>
+          <span> Phone Number </span>
+        </div> */}
+        <label htmlFor="name">Name: </label> 
+        <input id="name" type="text" placeholder="Type Text" defaultValue="Abhishek"></input>
       </div>
     );
   }
